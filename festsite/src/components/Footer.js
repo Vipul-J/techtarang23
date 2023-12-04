a99acf268f89dd91122e823e81435eab469c989e
Vipul-J <jujarvipul21@gmail.com>
2023-12-04 11:28:27 +0530

    final changes

--- festsite/src/components/Footer.js
+++ festsite/src/components/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
         <>
         <footer className="bg-light text-center border-top text-lg-start p-3">
             <div className="text-center rwt-txt-dark-blue ">
-                &copy; 2023, Designed and Developed by
-                <Link  to="/team"
-                    className="foot-link text-decoration-none fw-bold"> WuShang Clan</Link>
+                &copy; 2023,
+                <Link  to="/"
+                    className="foot-link text-decoration-none fw-bold"> Abhigyan1.0</Link>
             </div>
         </footer> 
         </>
